63a1cb67c69f69d60b9302e177eeecd404d27d8b
taschekuller <idogukanince@gmail.com>
2025-06-01 15:00:38 +0300

    Header :: add profile photo and route

--- quickq-app/components/Header/Header.tsx
+++ quickq-app/components/Header/Header.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { useNavigation } from 'expo-router';
+import { useNavigation, useRouter } from 'expo-router';
 import { DrawerActions } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Menu, Bell, Cat } from '@tamagui/lucide-icons';
-import { Button } from 'tamagui';
+import { Avatar, Button, XStack } from 'tamagui';
 
 
 
 export default function Header() {
   const navigation = useNavigation();
+  const router = useRouter();
   const insets = useSafeAreaInsets();
 
   const openDrawer = () => {
@@ -36,21 +37,28 @@ export default function Header() {
             QuickQ
           </Text>
         </View>
-        <View style={{ flexDirection: 'row', alignItems: 'center', margin: 0, padding: 0 }}>
-          <Button
-            circular
-            scaleIcon={1.5}
-            icon={Bell}
-            color={"#e1e1e1"}
-            style={{ marginBottom: 10, backgroundColor: "none" }}
-          />
-          <Button
-            circular
-            scaleIcon={1.5}
-            icon={Cat}
-            color={"#e1e1e1"}
-            style={{ marginBottom: 10, backgroundColor: "none" }}
-          />
+        <View>
+          <XStack
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Button
+              circular
+              scaleIcon={1.5}
+              icon={Bell}
+              color={"#e1e1e1"}
+              style={{ backgroundColor: "none" }}
+            />
+            <Button
+              circular
+              backgroundColor={"none"}
+              onPress={() => router.push('/(app)/tabs/(profile)')}
+            >
+              <Avatar size="$2" alignSelf="center">
+                <Avatar.Image src="https://avatar.iran.liara.run/public/1" />
+              </Avatar>
+            </Button>
+          </XStack>
         </View>
       </View>
     </SafeAreaView>
